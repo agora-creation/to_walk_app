599aa870af929099d4cc20c78032ad92e4b982fb
agoracreation <apple@agora-c.com>
2023-03-09 18:38:02 +0900

    20230309 commit

---
+++ lib/widgets/setting_card.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class SettingCard extends StatelessWidget {
+  final String labelText;
+  final List<Widget> children;
+
+  const SettingCard({
+    required this.labelText,
+    required this.children,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 8,
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Text(labelText),
+            ),
+            const Divider(),
+            Column(children: children),
+          ],
+        ),
+      ),
+    );
+  }
+}
