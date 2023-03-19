4fbe08b352e75011efc68ec7702c3023971b1e57
shimamura <charakuri8@gmail.com>
2023-03-19 10:51:22 +0900

    202303019 commit

--- lib/screens/room.dart
+++ lib/screens/room.dart
@@ -14,29 +14,41 @@ class RoomScreen extends StatelessWidget {
     final stepsProvider = Provider.of<StepsProvider>(context);
     final userProvider = Provider.of<UserProvider>(context);
 
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
+    return Stack(
       children: [
-        StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-          stream: stepsProvider.streamListNow(userId: userProvider.user?.id),
-          builder: (context, snapshot) {
-            int nowSteps = 0;
-            if (snapshot.hasData) {
-              for (DocumentSnapshot<Map<String, dynamic>> doc
-                  in snapshot.data!.docs) {
-                nowSteps += StepsModel.fromSnapshot(doc).stepsNum;
-              }
-            }
-            return StepsText(steps: nowSteps);
-          },
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+              stream: stepsProvider.streamListNow(
+                userId: userProvider.user?.id,
+              ),
+              builder: (context, snapshot) {
+                int nowSteps = 0;
+                if (snapshot.hasData) {
+                  for (DocumentSnapshot<Map<String, dynamic>> doc
+                      in snapshot.data!.docs) {
+                    nowSteps += StepsModel.fromSnapshot(doc).stepsNum;
+                  }
+                }
+                return StepsText(steps: nowSteps);
+              },
+            ),
+            Container(),
+            const Text(
+              '歩くと何かが起こるかも？',
+              style: TextStyle(
+                color: Colors.black38,
+                fontSize: 20,
+              ),
+            ),
+          ],
         ),
-        Container(),
-        const Text(
-          '歩くと何かが起こるかも？',
-          style: TextStyle(
-            color: Colors.black38,
-            fontSize: 20,
+        Center(
+          child: Image.asset(
+            'assets/images/alk_0.png',
+            fit: BoxFit.fitWidth,
           ),
         ),
       ],
