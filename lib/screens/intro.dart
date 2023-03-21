d23aced2f782b7ab7d78170b91867441e8adefef
shimamura <charakuri8@gmail.com>
2023-03-21 12:23:23 +0900

    202303021 commit

--- lib/screens/intro.dart
+++ lib/screens/intro.dart
@@ -4,6 +4,8 @@ import 'package:provider/provider.dart';
 import 'package:to_walk_app/helpers/functions.dart';
 import 'package:to_walk_app/providers/user.dart';
 import 'package:to_walk_app/screens/home.dart';
+import 'package:to_walk_app/widgets/custom_text_button.dart';
+import 'package:to_walk_app/widgets/custom_text_form_field.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
 class IntroScreen extends StatefulWidget {
@@ -55,9 +57,23 @@ class _IntroScreenState extends State<IntroScreen> {
                   ),
                 ],
               ),
-              bodyWidget: TextButton(
-                onPressed: () {},
-                child: const Text('引き継ぐ'),
+              bodyWidget: Column(
+                children: [
+                  const SizedBox(height: 80),
+                  CustomTextButton(
+                    labelText: '既に遊んだことがある場合',
+                    backgroundColor: Colors.cyan.shade600,
+                    onPressed: () {
+                      showDialog(
+                        barrierDismissible: false,
+                        context: context,
+                        builder: (_) => MigrationDialog(
+                          userProvider: userProvider,
+                        ),
+                      );
+                    },
+                  ),
+                ],
               ),
             ),
             PageViewModel(
@@ -66,12 +82,26 @@ class _IntroScreenState extends State<IntroScreen> {
               image: Image.asset('assets/images/loading.png'),
             ),
             PageViewModel(
-              title: 'あなたの情報を教えてください',
-              body: '3ページ目だよ!',
+              title: 'あなたの名前を教えてください',
+              bodyWidget: Column(
+                children: const [
+                  Text('name'),
+                  Text('name'),
+                  Text('name'),
+                  Text('name'),
+                  Text('name'),
+                ],
+              ),
             ),
             PageViewModel(
               title: '最後に利用規約に同意してください',
-              bodyWidget: WebViewWidget(controller: controller),
+              bodyWidget: SizedBox(
+                height: 350,
+                child: Scrollbar(
+                  controller: ScrollController(),
+                  child: WebViewWidget(controller: controller),
+                ),
+              ),
             ),
           ],
           onDone: () async {
@@ -80,19 +110,22 @@ class _IntroScreenState extends State<IntroScreen> {
             if (!mounted) return;
             pushReplacementScreen(context, const HomeScreen());
           },
-          showBackButton: true,
+          showBackButton: false,
           next: const Icon(Icons.chevron_right),
           back: const Icon(Icons.chevron_left),
           done: const Text(
             '同意する',
-            style: TextStyle(fontWeight: FontWeight.w600),
+            style: TextStyle(color: Colors.white),
+          ),
+          doneStyle: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all<Color>(
+              Colors.cyan.shade600,
+            ),
           ),
           dotsDecorator: DotsDecorator(
             size: const Size.square(10.0),
-            // ここの大きさを変更することで
-            // 現在の位置を表しているマーカーのUIが変更するよ!
             activeSize: const Size(20.0, 10.0),
-            activeColor: Colors.blue,
+            activeColor: Colors.cyan.shade600,
             color: Colors.black26,
             spacing: const EdgeInsets.symmetric(horizontal: 3.0),
             activeShape: RoundedRectangleBorder(
@@ -100,6 +133,76 @@ class _IntroScreenState extends State<IntroScreen> {
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class MigrationDialog extends StatefulWidget {
+  final UserProvider userProvider;
+
+  const MigrationDialog({
+    required this.userProvider,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<MigrationDialog> createState() => _MigrationDialogState();
+}
+
+class _MigrationDialogState extends State<MigrationDialog> {
+  TextEditingController code = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'このアプリを別のスマートフォンで遊んだことがある場合、データを引き継いで始めることが可能です。',
+          ),
+          const SizedBox(height: 4),
+          const Text(
+            '別のスマートフォンでこのアプリを開いて、引き継ぎ用のコードをメモしていただき、以下に入力してください。',
+          ),
+          const SizedBox(height: 16),
+          const Text(
+            '引き継ぎ用のコード',
+            style: TextStyle(
+              color: Colors.black54,
+              fontSize: 14,
+            ),
+          ),
+          const SizedBox(height: 8),
+          CustomTextFormField(
+            controller: code,
+            obscureText: false,
+            keyboardType: TextInputType.visiblePassword,
+          ),
+          const SizedBox(height: 16),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              CustomTextButton(
+                labelText: 'やめる',
+                backgroundColor: Colors.grey,
+                onPressed: () => Navigator.pop(context),
+              ),
+              CustomTextButton(
+                labelText: '引き継いで始める',
+                backgroundColor: Colors.cyan.shade600,
+                onPressed: () async {
+                  String? error = await widget.userProvider.signIn();
+                  if (error != null) return;
+                  if (!mounted) return;
+                  pushReplacementScreen(context, const HomeScreen());
+                },
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
