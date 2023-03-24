70bc5168fce9626b07194093028dea1d65155e80
agoracreation <apple@agora-c.com>
2023-03-24 18:12:25 +0900

    20230324 commit

--- lib/games/first.dart
+++ lib/games/first.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/game.dart';
@@ -5,6 +7,16 @@ import 'package:flame/palette.dart';
 import 'package:flutter/material.dart';
 
 class FirstGame extends FlameGame with TapDetector {
+  @override
+  bool debugMode = false;
+
+  final TextPaint textPaint = TextPaint(
+    style: const TextStyle(
+      fontSize: 14,
+      fontFamily: 'TsunagiGothic',
+    ),
+  );
+
   @override
   void onTapUp(TapUpInfo info) {
     final touchPoint = info.eventPosition.game;
@@ -19,16 +31,27 @@ class FirstGame extends FlameGame with TapDetector {
       add(Square()
         ..position = touchPoint
         ..squareSize = 45.0
-        ..velocity = Vector2(0, 1).normalized() * 50
+        ..velocity = Vector2(0, 1).normalized() * 25
         ..color = (BasicPalette.red.paint()
           ..style = PaintingStyle.stroke
           ..strokeWidth = 2));
     }
   }
+
+  @override
+  void render(Canvas canvas) {
+    textPaint.render(
+      canvas,
+      "四角はいま${children.length}",
+      Vector2(10, 20),
+    );
+    super.render(canvas);
+  }
 }
 
 class Square extends PositionComponent {
-  var velocity = Vector2(0, 0).normalized() * 25;
+  var velocity = Vector2(0, 25);
+  var rotationSpeed = 0.3;
   var squareSize = 128.0;
   var color = BasicPalette.white.paint()
     ..style = PaintingStyle.stroke
@@ -38,13 +61,15 @@ class Square extends PositionComponent {
   Future onLoad() async {
     super.onLoad();
     size.setValues(squareSize, squareSize);
-    anchor = Anchor.topRight;
+    anchor = Anchor.bottomCenter;
   }
 
   @override
   void update(double dt) {
     super.update(dt);
     position += velocity * dt;
+    var angleDelta = dt * rotationSpeed;
+    angle = (angle - angleDelta) % (2 * pi);
   }
 
   @override
